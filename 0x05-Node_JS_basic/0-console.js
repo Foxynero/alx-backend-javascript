d9c7249de90c9753df0d2f3114b4673005fce704
FOXYNERO <113718153+Foxynero@users.noreply.github.com>
2023-09-19 19:55:51 +0000

    Delete 0x05-Node_JS_basic directory

--- 0x05-Node_JS_basic/0-console.js
+++
@@ -1,9 +0,0 @@
-/**
- * Logs output to the STDOUT
- * @param {Strin} arg The argument to display
- * @returns
- */
-const displayMessage = (arg) => {
-  console.log(arg);
-};
-module.exports = displayMessage;
